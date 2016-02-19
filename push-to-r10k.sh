b4935406279026bdf3bf373b7e6af8a25455f145
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-02-19 12:33:54 +0100

    Get REF from webhoo

--- push-to-r10k.sh
+++ push-to-r10k.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+REF="$1"
+
 export PATH=/opt/puppetlabs/bin:$PATH
-while read oldrev newrev refname; do
-  # R10K
-  if [[ $refname =~ 'refs/heads/' ]]; then
-    branch=$(git rev-parse --symbolic --abbrev-ref $refname)
-    mco r10k deploy $branch
-  else
-    echo "r10k skipping $refname"
-  fi
-done
+
+if [[ $REF =~ 'refs/heads/' ]]; then
+  branch=$(cut -d/ -f3 <<<"${REF}")
+  mco r10k deploy $branch
+else
+  echo "r10k skipping $REF"
+fi
