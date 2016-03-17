04042dd4949b8a5fc30b6ad16340f8cc68b2dc9b
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-03-17 09:19:34 +0100

    Allow to set mcollective's password

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 sed -i "s/%{HOOKS_SECRET}/${HOOKS_SECRET}/g" /etc/webhook/*.json
+sed -i -e "s/^\(plugin.activemq.pool.1.password = \).*$/\1${MCOLLECTIVE_PASSWORD}/" /etc/puppetlabs/mcollective/client.cfg
 
 exec /go/bin/webhook -hooks /etc/webhook/*.json -verbose
