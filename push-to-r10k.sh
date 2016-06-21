ceb4190a4ab4b5984ca0f77224f20b0b5b16c45c
Jeanneret Cedric <cjeanneret@users.noreply.github.com>
2016-06-21 10:47:36 +0200

    Replaced deploy by sync for mco r10k

--- push-to-r10k.sh
+++ push-to-r10k.sh
@@ -5,8 +5,7 @@ REF="$1"
 export PATH=/opt/puppetlabs/bin:$PATH
 
 if [[ $REF =~ 'refs/heads/' ]]; then
-  branch=$(cut -d/ -f3 <<<"${REF}")
-  mco r10k deploy $branch
+  mco r10k sync
 else
   echo "r10k skipping $REF"
 fi
